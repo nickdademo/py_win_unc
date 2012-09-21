7b9bca83cf4e973a0fc59c52fba0d6958112a4e5
3noch <elliot@3noch.com>
2012-09-21 14:30:23 -0400

    Add sanitization funcion for file names

--- test/test_sanitize.py
+++ test/test_sanitize.py
@@ -1,21 +1,34 @@
 from unittest import TestCase
 
-from win_unc.internal.sanitize import sanitize_for_shell, sanitize_logon, sanitize_path
+from win_unc.internal import sanitize as lib
 
 
 class TestSanitize(TestCase):
     def test_sanitize_for_shell(self):
-        self.assertEqual(sanitize_for_shell(''), '')
-        self.assertEqual(sanitize_for_shell('"'), r'\"')
-        self.assertEqual(sanitize_for_shell('abc'), 'abc')
-        self.assertEqual(sanitize_for_shell('abc"""'), r'abc\"\"\"')
+        self.assertEqual(lib.sanitize_for_shell(''), '')
+        self.assertEqual(lib.sanitize_for_shell('abcABC'), 'abcABC')
+        self.assertEqual(lib.sanitize_for_shell('"'), r'\"')
+        self.assertEqual(lib.sanitize_for_shell('abc"""'), r'abc\"\"\"')
 
     def test_sanitize_for_logon(self):
-        self.assertEqual(sanitize_logon(''), '')
+        self.assertEqual(lib.sanitize_logon(''), '')
+        self.assertEqual(lib.sanitize_logon('abcABC'), 'abcABC')
+        self.assertEqual(lib.sanitize_logon(r'"/[]:;|=,+*?<>'), '')
+        self.assertEqual(lib.sanitize_logon(r'"/[]:;|=,+*?<>'), '')
+        self.assertEqual(lib.sanitize_logon('\0'), '')
 
     def test_sanitize_path(self):
-        self.assertEqual(sanitize_path(''), '')
-        self.assertEqual(sanitize_path(r'C:\a valid\folder'), r'C:\a valid\folder')
-        self.assertEqual(sanitize_path(r':\\'), r':\\')
-        self.assertEqual(sanitize_path(r'<>"/|?*'), '')
-        self.assertEqual(sanitize_path('\0\1\2\3\4\30\31'), '')
+        self.assertEqual(lib.sanitize_path(''), '')
+        self.assertEqual(lib.sanitize_path('abcABC'), 'abcABC')
+        self.assertEqual(lib.sanitize_path(r'C:\a valid\folder'), r'C:\a valid\folder')
+        self.assertEqual(lib.sanitize_path(r':\\'), r':\\')
+        self.assertEqual(lib.sanitize_path(r'<>"/|?*'), '')
+        self.assertEqual(lib.sanitize_path('\0\1\2\3\4\30\31'), '')
+
+    def test_sanitize_file_name(self):
+        self.assertEqual(lib.sanitize_file_name(''), '')
+        self.assertEqual(lib.sanitize_file_name('abcABC'), 'abcABC')
+        self.assertEqual(lib.sanitize_file_name(r'C:\a valid\folder'), r'Ca validfolder')
+        self.assertEqual(lib.sanitize_file_name(r':\\'), r'')
+        self.assertEqual(lib.sanitize_file_name(r'<>"/|?*'), '')
+        self.assertEqual(lib.sanitize_file_name('\0\1\2\3\4\30\31'), '')
--- win_unc/internal/sanitize.py
+++ win_unc/internal/sanitize.py
@@ -23,3 +23,10 @@ def sanitize_path(path):
     Removes characters from `path` that cannot be part of a valid Windows path.
     """
     return path.translate(None, r'<>"/|?*' + ''.join(map(chr, range(0, 31))))
+
+
+def sanitize_file_name(file_name):
+    """
+    Removes characters from `file_name` that cannot be part of a valid Windows file name.
+    """
+    return sanitize_path(file_name).translate(None, ':\\')
