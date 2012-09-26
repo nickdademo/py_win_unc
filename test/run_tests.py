7812934504c299464227c4f8cd28cacc2b904008
3noch <elliot@3noch.com>
2012-09-26 08:45:35 -0400

    Use spaces instead of tabs

--- test/run_tests.py
+++ test/run_tests.py
@@ -8,10 +8,10 @@ from test_unc_directory import *
 
 
 if platform.system() == 'Windows':
-	print 'Including Windows-specific tests'
-	from test_connecting import *
+    print 'Including Windows-specific tests'
+    from test_connecting import *
 else:
-	print 'WARNING: Excluding Windows-specific tests because host is not Windows'
+    print 'WARNING: Excluding Windows-specific tests because host is not Windows'
 
 
 if __name__ == '__main__':
