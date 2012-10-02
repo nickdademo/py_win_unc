07058595e43290524d28b53b5919fb76f16c618b
3noch <elliot@3noch.com>
2012-10-02 18:02:22 -0400

    Add tests for UNC path validator

--- test/test_validators.py
+++ test/test_validators.py
@@ -15,3 +15,17 @@ class TestIsValidDriveLetter(TestCase):
         self.assertFalse(V.is_valid_drive_letter(':'))
         self.assertFalse(V.is_valid_drive_letter('aa'))
         self.assertFalse(V.is_valid_drive_letter('a:'))
+
+
+class TestIsValidUncPath(TestCase):
+    def test_valid(self):
+        self.assertTrue(V.is_valid_unc_path(r'\\a'))
+        self.assertTrue(V.is_valid_unc_path(r'\\a\b\c'))
+        self.assertTrue(V.is_valid_unc_path(r'\\ABC\\'))
+
+    def test_invalid(self):
+        self.assertFalse(V.is_valid_unc_path(''))
+        self.assertFalse(V.is_valid_unc_path(r'\\'))
+        self.assertFalse(V.is_valid_unc_path(r'\\\a'))
+        self.assertFalse(V.is_valid_unc_path(r'C:\path'))
+        self.assertFalse(V.is_valid_unc_path(r'\\<a>'))
