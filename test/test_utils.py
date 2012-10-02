e903d6bde05f2145f1aae89ba4e6452958a07723
3noch <elliot@3noch.com>
2012-10-02 18:04:26 -0400

    Standardize import style in tests

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from win_unc.internal import utils
+from win_unc.internal import utils as U
 
 
 never = lambda _: False
@@ -15,73 +15,73 @@ class ListTransformerTestCase(TestCase):
 
 class TestDropWhile(ListTransformerTestCase):
     def test_drop_nothing(self):
-        self.assertEqualAsLists(utils.drop_while(never, ''), '')
-        self.assertEqualAsLists(utils.drop_while(never, 'a'), 'a')
-        self.assertEqualAsLists(utils.drop_while(never, 'abc'), 'abc')
+        self.assertEqualAsLists(U.drop_while(never, ''), '')
+        self.assertEqualAsLists(U.drop_while(never, 'a'), 'a')
+        self.assertEqualAsLists(U.drop_while(never, 'abc'), 'abc')
 
     def test_drop_everything(self):
-        self.assertEqualAsLists(utils.drop_while(always, ''), '')
-        self.assertEqualAsLists(utils.drop_while(always, 'a'), '')
-        self.assertEqualAsLists(utils.drop_while(always, 'abc'), '')
+        self.assertEqualAsLists(U.drop_while(always, ''), '')
+        self.assertEqualAsLists(U.drop_while(always, 'a'), '')
+        self.assertEqualAsLists(U.drop_while(always, 'abc'), '')
 
     def test_drop_with_predicate(self):
         not_alpha = lambda x: not x.isalpha()
-        self.assertEqualAsLists(utils.drop_while(not_alpha, ''), '')
-        self.assertEqualAsLists(utils.drop_while(not_alpha, 'abc'), 'abc')
-        self.assertEqualAsLists(utils.drop_while(not_alpha, '123abc456'), 'abc456')
-        self.assertEqualAsLists(utils.drop_while(not_alpha, '   abc   '), 'abc   ')
-        self.assertEqualAsLists(utils.drop_while(not_alpha, '   '), '')
+        self.assertEqualAsLists(U.drop_while(not_alpha, ''), '')
+        self.assertEqualAsLists(U.drop_while(not_alpha, 'abc'), 'abc')
+        self.assertEqualAsLists(U.drop_while(not_alpha, '123abc456'), 'abc456')
+        self.assertEqualAsLists(U.drop_while(not_alpha, '   abc   '), 'abc   ')
+        self.assertEqualAsLists(U.drop_while(not_alpha, '   '), '')
 
 
 class TestTakeWhile(ListTransformerTestCase):
     def test_take_nothing(self):
-        self.assertEqualAsLists(utils.take_while(never, ''), '')
-        self.assertEqualAsLists(utils.take_while(never, 'a'), '')
-        self.assertEqualAsLists(utils.take_while(never, 'abc'), '')
+        self.assertEqualAsLists(U.take_while(never, ''), '')
+        self.assertEqualAsLists(U.take_while(never, 'a'), '')
+        self.assertEqualAsLists(U.take_while(never, 'abc'), '')
 
     def test_take_everything(self):
-        self.assertEqualAsLists(utils.take_while(always, ''), '')
-        self.assertEqualAsLists(utils.take_while(always, 'a'), 'a')
-        self.assertEqualAsLists(utils.take_while(always, 'abc'), 'abc')
+        self.assertEqualAsLists(U.take_while(always, ''), '')
+        self.assertEqualAsLists(U.take_while(always, 'a'), 'a')
+        self.assertEqualAsLists(U.take_while(always, 'abc'), 'abc')
 
     def test_take_with_predicate(self):
-        self.assertEqualAsLists(utils.take_while(is_alpha, ''), '')
-        self.assertEqualAsLists(utils.take_while(is_alpha, 'abc'), 'abc')
-        self.assertEqualAsLists(utils.take_while(is_alpha, '123abc456'), '')
-        self.assertEqualAsLists(utils.take_while(is_alpha, '   abc   '), '')
-        self.assertEqualAsLists(utils.take_while(is_alpha, '   '), '')
+        self.assertEqualAsLists(U.take_while(is_alpha, ''), '')
+        self.assertEqualAsLists(U.take_while(is_alpha, 'abc'), 'abc')
+        self.assertEqualAsLists(U.take_while(is_alpha, '123abc456'), '')
+        self.assertEqualAsLists(U.take_while(is_alpha, '   abc   '), '')
+        self.assertEqualAsLists(U.take_while(is_alpha, '   '), '')
 
 
 class TestFirst(TestCase):
     def test_no_match(self):
-        self.assertEqual(utils.first(never, ''), None)
-        self.assertEqual(utils.first(never, 'abc'), None)
+        self.assertEqual(U.first(never, ''), None)
+        self.assertEqual(U.first(never, 'abc'), None)
 
     def test_all_match(self):
-        self.assertEqual(utils.first(always, ''), None)
-        self.assertEqual(utils.first(always, 'abc'), 'a')
+        self.assertEqual(U.first(always, ''), None)
+        self.assertEqual(U.first(always, 'abc'), 'a')
 
     def test_with_predicate(self):
-        self.assertEqual(utils.first(is_alpha, ''), None)
-        self.assertEqual(utils.first(is_alpha, 'abc'), 'a')
-        self.assertEqual(utils.first(is_alpha, '123abc'), 'a')
-        self.assertEqual(utils.first(is_alpha, '   abc'), 'a')
+        self.assertEqual(U.first(is_alpha, ''), None)
+        self.assertEqual(U.first(is_alpha, 'abc'), 'a')
+        self.assertEqual(U.first(is_alpha, '123abc'), 'a')
+        self.assertEqual(U.first(is_alpha, '   abc'), 'a')
 
 
 class TestReversedFirst(TestCase):
     def test_no_match(self):
-        self.assertEqual(utils.rfirst(never, ''), None)
-        self.assertEqual(utils.rfirst(never, 'abc'), None)
+        self.assertEqual(U.rfirst(never, ''), None)
+        self.assertEqual(U.rfirst(never, 'abc'), None)
 
     def test_all_match(self):
-        self.assertEqual(utils.rfirst(always, ''), None)
-        self.assertEqual(utils.rfirst(always, 'abc'), 'c')
+        self.assertEqual(U.rfirst(always, ''), None)
+        self.assertEqual(U.rfirst(always, 'abc'), 'c')
 
     def test_with_predicate(self):
-        self.assertEqual(utils.rfirst(is_alpha, ''), None)
-        self.assertEqual(utils.rfirst(is_alpha, 'abc'), 'c')
-        self.assertEqual(utils.rfirst(is_alpha, 'abc123'), 'c')
-        self.assertEqual(utils.rfirst(is_alpha, 'abc   '), 'c')
+        self.assertEqual(U.rfirst(is_alpha, ''), None)
+        self.assertEqual(U.rfirst(is_alpha, 'abc'), 'c')
+        self.assertEqual(U.rfirst(is_alpha, 'abc123'), 'c')
+        self.assertEqual(U.rfirst(is_alpha, 'abc   '), 'c')
 
 
 class TestHigherOrderNot(TestCase):
@@ -90,11 +90,11 @@ class TestHigherOrderNot(TestCase):
         true_ = lambda: True
         id_ = lambda x: x
 
-        self.assertEqual(utils.not_(false_)(), True)
-        self.assertEqual(utils.not_(id_)(False), True)
+        self.assertEqual(U.not_(false_)(), True)
+        self.assertEqual(U.not_(id_)(False), True)
 
-        self.assertEqual(utils.not_(true_)(), False)
-        self.assertEqual(utils.not_(id_)(True), False)
+        self.assertEqual(U.not_(true_)(), False)
+        self.assertEqual(U.not_(id_)(True), False)
 
 
 def raiseSomething(exception):
@@ -106,10 +106,10 @@ class TestCatch(TestCase):
         self.assertEqual(typeId, type(b))
 
     def test_catch(self):
-        self.assertIsType(Exception, utils.catch(raiseSomething, Exception))
-        self.assertIsType(ValueError, utils.catch(raiseSomething, ValueError))
-        self.assertIsType(Exception, utils.catch(self.raiseSomething, Exception))
-        self.assertIsType(ValueError, utils.catch(self.raiseSomething, ValueError))
+        self.assertIsType(Exception, U.catch(raiseSomething, Exception))
+        self.assertIsType(ValueError, U.catch(raiseSomething, ValueError))
+        self.assertIsType(Exception, U.catch(self.raiseSomething, Exception))
+        self.assertIsType(ValueError, U.catch(self.raiseSomething, ValueError))
 
     def raiseSomething(self, exception):
         raise exception()
