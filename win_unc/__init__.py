f0afb654452de05437899067c994e008c58f7022
3noch <elliot@3noch.com>
2012-10-05 16:11:16 -0400

    Export most common classes directly

--- win_unc/__init__.py
+++ win_unc/__init__.py
@@ -1,4 +1,14 @@
+from win_unc.connecting import UncDirectoryConnection, UncDirectoryMount
+from win_unc.disk_drive import DiskDrive
+from win_unc.unc_credentials import UncCredentials
+from win_unc.unc_directory import UncDirectory
+
+
 __version__ = '0.1'
 VERSION = tuple(map(int, __version__.split('.')))
 
-__all__ = []
+__all__ = ['UncDirectoryConnection',
+           'UncDirectoryMount',
+           'DiskDrive',
+           'UncCredentials',
+           'UncDirectory']
