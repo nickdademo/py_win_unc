7137b4ec5ed77069c11b9206f134a1f1fd653574
3noch <elliot@3noch.com>
2012-10-08 18:42:32 -0400

    Add docstrings to validators

--- win_unc/validators.py
+++ win_unc/validators.py
@@ -7,11 +7,20 @@ from win_unc.sanitizors import sanitize_username, sanitize_unc_path
 
 
 def is_valid_drive_letter(string):
+    """
+    Returns `True` if `string` represents a valid drive letter. Drive letters are exactly one
+    character in length and between "A" and "Z". Case does not matter.
+    """
     return (len(string) == 1          # Must be exactly one character long
             and string[0].isalpha())  # Must be an alphabet character (A through Z)
 
 
 def is_valid_unc_path(string):
+    """
+    Returns `True` if `string` is a valid UNC path. Valid UNC paths are at least three characters
+    long, begin with "\\", do not start or end with whitepsace, and do not contain certain invalid
+    characters (see `sanitize_unc_path`).
+    """
     return (len(string) > 2                           # Must be at least 3 characters
             and take_while(lambda x: x == '\\', string) == ['\\', '\\']  # Must begin with `\\`
             and string == string.strip()              # Must not have surrounding white-space
@@ -20,8 +29,9 @@ def is_valid_unc_path(string):
 
 def is_valid_username(string):
     """
-    Returns `True` for valid Windows usernames (logons). A valid username is a non-empty string
-    without certain characters (see `sanitize_username`).
+    Returns `True` for valid Windows usernames (logons). A valid username is a non-empty string,
+    that does not start or end with whitespace, and does not contain certain invalid characters
+    (see `sanitize_username`).
     """
     return (len(string) > 0                           # Must not be empty
             and string == string.strip()              # Must not have surrounding white-space
