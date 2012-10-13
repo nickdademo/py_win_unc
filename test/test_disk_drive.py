e3aa887541cb6e60c52f7218dbba1246c4a7c069
3noch <elliot@3noch.com>
2012-10-13 19:48:45 -0400

    Forgot some colons

--- test/test_disk_drive.py
+++ test/test_disk_drive.py
@@ -6,14 +6,14 @@ from win_unc.disk_drive import DiskDrive
 
 class TestDiskDrive(TestCase):
     def test_init_with_valid_string(self):
-        self.assertEqual(DiskDrive('a').get_drive(), 'A')
-        self.assertEqual(DiskDrive('A').get_drive(), 'A')
-        self.assertEqual(DiskDrive('A:').get_drive(), 'A')
-        self.assertEqual(DiskDrive('a:').get_drive(), 'A')
-        self.assertEqual(DiskDrive('a').get_drive(), 'A')
-        self.assertEqual(DiskDrive('A:\\').get_drive(), 'A')
-        self.assertEqual(DiskDrive('a:\\').get_drive(), 'A')
-        self.assertEqual(DiskDrive('Z:').get_drive(), 'Z')
+        self.assertEqual(DiskDrive('a').get_drive(), 'A:')
+        self.assertEqual(DiskDrive('A').get_drive(), 'A:')
+        self.assertEqual(DiskDrive('A:').get_drive(), 'A:')
+        self.assertEqual(DiskDrive('a:').get_drive(), 'A:')
+        self.assertEqual(DiskDrive('a').get_drive(), 'A:')
+        self.assertEqual(DiskDrive('A:\\').get_drive(), 'A:')
+        self.assertEqual(DiskDrive('a:\\').get_drive(), 'A:')
+        self.assertEqual(DiskDrive('Z:').get_drive(), 'Z:')
 
     def test_init_with_invalid_string(self):
         self.assertRaises(InvalidDiskDriveError, DiskDrive, '')
@@ -24,8 +24,8 @@ class TestDiskDrive(TestCase):
         self.assertRaises(InvalidDiskDriveError, DiskDrive, 'abc')
 
     def test_init_for_clone(self):
-        self.assertEqual(DiskDrive(DiskDrive('A:')).get_drive(), 'A')
-        self.assertEqual(DiskDrive(DiskDrive('Z:')).get_drive(), 'Z')
+        self.assertEqual(DiskDrive(DiskDrive('A:')).get_drive(), 'A:')
+        self.assertEqual(DiskDrive(DiskDrive('Z:')).get_drive(), 'Z:')
 
     def test_eq(self):
         self.assertEqual(DiskDrive('A:'), DiskDrive('A:'))
