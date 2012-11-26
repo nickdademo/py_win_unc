cf52fa883bcc2b7fd95b251aefd200ca89d9c295
3noch <elliot@3noch.com>
2012-11-26 10:29:20 -0500

    Refactor connection tests

---
+++ test/connection_test_utils.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from win_unc.internal.shell import run
+from win_unc.unc_directory import UncDirectory
+from win_unc.internal.current_state import get_current_net_use_table
+
+
+LOCALHOST_UNC = r'\\localhost\IPC$'
+
+
+class LocalHostConnectionTest(TestCase):
+    def setUp(self):
+        self.localhost_was_connected = self.localhost_connected()
+        if self.localhost_was_connected:
+            run('NET USE ' + LOCALHOST_UNC + ' /DELETE')
+
+    def tearDown(self):
+        if self.localhost_was_connected:
+            run('NET USE ' + LOCALHOST_UNC)
+
+    def localhost_connected(self):
+        net_use = get_current_net_use_table()
+        return UncDirectory(LOCALHOST_UNC) in net_use.get_connected_paths()
--- test/win_test_connecting.py
+++ test/win_test_connecting.py
@@ -1,24 +1,10 @@
-from unittest import TestCase
-
 from win_unc.connecting import UncDirectoryConnection
-from win_unc.internal.shell import run
 from win_unc.unc_directory import UncDirectory
-from win_unc.internal.current_state import get_current_net_use_table
+
+from connection_test_utils import LocalHostConnectionTest
 
 
-LOCALHOST_UNC = r'\\localhost\IPC$'
-
-
-class TestUncDirectoryConnection(TestCase):
-    def setUp(self):
-        self.localhost_was_connected = self.localhost_connected()
-        if self.localhost_was_connected:
-            run('NET USE ' + LOCALHOST_UNC + ' /DELETE')
-
-    def tearDown(self):
-        if self.localhost_was_connected:
-            run('NET USE ' + LOCALHOST_UNC)
-
+class TestUncDirectoryConnection(LocalHostConnectionTest):
     def test_connect(self):
         unc = UncDirectory(r'\\localhost')
         conn = UncDirectoryConnection(unc)
@@ -31,7 +17,3 @@ class TestUncDirectoryConnection(TestCase):
 
         self.assertFalse(self.localhost_connected())
         self.assertFalse(conn.is_connected())
-
-    def localhost_connected(self):
-        net_use = get_current_net_use_table()
-        return UncDirectory(LOCALHOST_UNC) in net_use.get_connected_paths()
