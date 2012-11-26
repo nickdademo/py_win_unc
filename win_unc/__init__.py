13dead76ff3a6606cb7138ba45f671c869f91172
3noch <elliot@3noch.com>
2012-11-26 12:53:27 -0500

    Update version to 0.5.0

--- win_unc/__init__.py
+++ win_unc/__init__.py
@@ -8,7 +8,7 @@ from win_unc.unc_credentials import UncCredentials
 from win_unc.unc_directory import UncDirectory
 
 
-__version__ = '0.4.1'
+__version__ = '0.5.0'
 VERSION = tuple(map(int, __version__.split('.')))
 
 __all__ = ['UncDirectoryConnection',
